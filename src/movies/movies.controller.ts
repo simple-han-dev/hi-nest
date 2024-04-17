275cd4c00a25172508a0c78bbe196dabd981c541
hanseunghyeon <shinhs9423@naver.com>
2024-04-17 20:41:07 +0900

    feat: controller constructor
    - movies service bind controller

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,9 +9,12 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
   getAll(): string {
     return 'This will return all movies';
