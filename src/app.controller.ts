399f4ea5383deb60a2864bd6a0d46cdf46948c9e
hanseunghyeon <shinhs9423@naver.com>
2024-04-17 13:34:46 +0900

    feat: add route
    - add route and controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from "@nestjs/common";
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,38 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  /*
+    Add a new route and handler
+    1. call decorator
+    2. defind path
+    3. define method
+    + Decorators and methods must have no spaces
+
+    @decorator(path)
+    method() {}
+
+    [sample]
+
+    @Get('/hello')
+    sayHello() {
+      return 'Hello World!';
+    }
+
+    @Post('/hello')
+    saveName() {
+      ...
+    }
+
+    express에서는 아래 처럼 사용했었음
+    app.get('/hello', (req, res) => {
+      ...
+    }
+  */
+
+  @Get('/hello')
+  sayHello(): string {
+    return 'Welcome to the NestJS API!';
   }
 }
