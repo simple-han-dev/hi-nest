67cc0b655e29685c8fcc43c7fad0d60dbc92592b
hanseunghyeon <shinhs9423@naver.com>
2024-04-17 20:37:48 +0900

    feat: create methods for movies

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -4,4 +4,21 @@ import { Movie } from './entities/movie.entity';
 @Injectable()
 export class MoviesService {
   private movies: Movie[] = [];
+
+  /**
+   * get all movies
+   * @return {array} [{}, {}, ...]
+   */
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  /**
+   * find movie by id
+   * @param {string} id
+   * @return {object} {}
+   */
+  getOne(id: string): Movie {
+    return this.movies.find((_movie) => _movie.id === Number(id));
+  }
 }
