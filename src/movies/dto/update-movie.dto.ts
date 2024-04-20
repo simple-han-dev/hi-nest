29c865c1908c78b53a4315a88b191b65e13b8207
hanseunghyeon <shinhs9423@naver.com>
2024-04-20 17:34:56 +0900

    refactor: extends type & optional
    - type extends
    - optional
    
    #814ae3a7

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,15 +1,4 @@
-import { IsNumber, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto {
-  @IsNumber()
-  readonly id: number;
-
-  @IsString()
-  readonly title?: string;
-
-  @IsNumber()
-  readonly year?: number;
-
-  @IsString({ each: true })
-  readonly genres?: string[];
-}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
