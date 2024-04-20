5d4ae32ba395e89e435df10ec8c3eeb5697ca405
hanseunghyeon <shinhs9423@naver.com>
2024-04-20 19:05:13 +0900

    test: movies.service add test
    - getAll()
    - getOne()
    
    #f840e5b7

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -15,4 +15,31 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('get all', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('get one', () => {
+    const _movieId = 1;
+    const _notFoundMovieId = 0;
+
+    it('should throw NotFoundException', () => {
+      try {
+        service.getOne(_notFoundMovieId);
+      } catch (e) {
+        expect(e).toBeDefined();
+        expect(e.message).toEqual(`Not Found Movie Id :${_notFoundMovieId}`);
+      }
+    });
+
+    it('should return a movie', () => {
+      const movie = service.getOne(_movieId);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(_movieId);
+    });
+  });
 });
